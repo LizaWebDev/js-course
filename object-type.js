77d37630825ba804aee0c0ea0683d19acb3c850e
LizaWebDev <e.kuntsevich03@gmail.com>
2022-07-04 19:15:06 +0300

    variables and object type

---
+++ object-type.js
@@ -0,0 +1,14 @@
+const myObject = {
+   name: 'Liza',
+   age: 17
+}
+
+const copyOfMyObject = myObject;
+
+copyOfMyObject.age = 18;
+
+console.log(myObject)
+
+myObject.city = 'Orsha';
+
+console.log(myObject)
---
+++ variables.js
@@ -0,0 +1,10 @@
+//variables
+let myName
+
+console.log(myName)
+
+myName = 'Liza'
+
+console.log(myName)
+
+console.log(console.log(10))
