ae17e84df0f31283d994df662fb2a03e464c7089
LizaWebDev <e.kuntsevich03@gmail.com>
2022-07-05 20:25:33 +0300

    object properties

---
+++ object-properties.js
@@ -0,0 +1,15 @@
+const myCity = {
+   city: 'New York',
+   popular: true
+}
+
+//dot notation
+
+console.log(myCity.popular)
+
+//bracket notation
+
+const countryPropertyName = 'country'
+myCity[countryPropertyName] = 'USA'
+
+console.log(myCity.country)
